59bb5ca785b58fe8e44a9cf888d7aaa3f283592a
Roman Leshin <vorzee@gmail.com>
2025-01-07 21:33:47 +0500

    Добавили тесты с JUnit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,12 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.junit.Test
     public void remain() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(900);
@@ -14,8 +14,24 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void remainBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainJUnit5() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainBoundaryJUnit5() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         int expected = 0;
