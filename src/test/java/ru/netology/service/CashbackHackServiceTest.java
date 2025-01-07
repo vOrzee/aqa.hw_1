645285a95f6efab941f66e9a523871a6845acf8e
Roman Leshin <vorzee@gmail.com>
2025-01-07 18:39:31 +0500

    Добавили простейший тест для TestNG, тест для пограничного значения будет добавлен в ветке JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,16 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+}
