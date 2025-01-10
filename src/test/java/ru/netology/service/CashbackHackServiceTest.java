897d75ac6c52694f3dc1472a3245e1f58c3e1cab
Roman Leshin <vorzee@gmail.com>
2025-01-10 18:27:18 +0500

    Добавили тест на пограничное значение

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,4 +13,12 @@ public class CashbackHackServiceTest {
         int expected = 100;
         assertEquals(actual, expected);
     }
+
+    @Test
+    public void remainBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
