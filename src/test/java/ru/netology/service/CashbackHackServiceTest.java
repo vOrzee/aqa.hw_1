04a06af3afdd9f15b967721dfb6b741a7a0c970a
Roman Leshin <vorzee@gmail.com>
2025-01-10 18:27:54 +0500

    Поправили очерёдность

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -19,6 +19,6 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
